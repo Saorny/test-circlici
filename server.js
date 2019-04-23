9dc55b0dfd34b20097becf48ac6d6e1aa3d8cbc2
Remi Bartel <remi.bartel@mobilenowgroup.com>
2019-04-23 11:13:06 +0800

    adding unit tests

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.static('styles'));
 
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
-  res.render('index.html');
+  res.json({res: "this is working!"});
 });
 
 //start a server on port 80 and log its start to our console
-const server = app.listen(80, function () {
+const server = app.listen(5000, function () {
   const port = server.address().port;
 
   console.log('Example app listening on port ', port);
---
+++ test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const assert = require('assert');
+const chai = require('chai');
+
+it('should return true', () => {
+  assert.equal(true, true)
+});
+
